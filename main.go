a4c8d2147f84db58e7370d3f83d2e24cdb218b70
andrea lin <andrea.lin@siriustech.io>
2023-02-19 11:57:58 -0300

    better folder structure and graceful shutdown

--- main.go
+++ main.go
@@ -1,35 +1,56 @@
 package main
 
 import (
-	"fmt"
-	"io"
+	"context"
+	"learn-go/handlers"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
-	// registers a http handler function to root path
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("hello world")
+	// create a new logger
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 
-		// read request body
-		d, err := io.ReadAll(r.Body)
+	// inject logger dependency to handlers
+	helloHandler := handlers.NewHello(l)
+	goodbyeHandler := handlers.NewGoodbye(l)
 
-		// if there is error while reading req body, server response with http error 400 and error message
+	// create new ServeMux
+	sm := http.NewServeMux()
+
+	// register handler to path
+	sm.Handle("/", helloHandler)
+	sm.Handle("/goodbye", goodbyeHandler)
+
+	// configure server
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	// listens on the TCP network address srv.Addr and then calls Serve to handle requests on incoming connections.
+	go func() {
+		err := s.ListenAndServe()
 		if err != nil {
-			http.Error(w, "Oops", http.StatusBadRequest)
-			// terminate the flow
-			return
+			l.Fatal(err)
 		}
+	}()
 
-		// write to response
-		fmt.Fprintf(w, "Hello %s", d)
-	})
+	// graceful shutdown
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt, os.Kill)
 
-	// registers a http handler function a /goodbye path
-	http.HandleFunc("/goodbye", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("goodbye world")
-	})
+	sig := <-signals
+	l.Println("received terminate, graceful shutdown", sig)
 
-	http.ListenAndServe(":9090", nil)
+	duration := time.Now().Add(30 * time.Second)
+	ctx, cancel := context.WithDeadline(context.Background(), duration)
+	defer cancel()
+	s.Shutdown(ctx)
 }
