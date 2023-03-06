6c73b1f8d67a78783abee1a1cefd9b99c9476c35
andrea lin <andrea.lin@siriustech.io>
2023-03-06 10:17:43 -0300

    feat: add swagger

---
+++ scripts/generate_swagger.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+alias swagger='docker run --rm -it  --user $(id -u):$(id -g) -e GOCACHE=/tmp -e  GOPATH=$(go env GOPATH):/go -v $HOME:$HOME -w $(pwd) quay.io/goswagger/swagger'
+swagger generate spec -o ./swagger.yaml --scan-models
